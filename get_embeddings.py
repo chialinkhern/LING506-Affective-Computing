aa1b6fb0939b3e19d15f9d6b08f91ab40defe7aa
chialinkhern <chialinkhern@gmail.com>
2022-12-08 11:48:56 -0600

    configured for GPU

--- get_embeddings.py
+++ get_embeddings.py
@@ -9,25 +9,29 @@ from datasets import load_dataset
 
 class FeatureExtractor:
     def __init__(self, tokenizer, model, sequence_of_input=None):
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.tokenizer = tokenizer
-        self.model = model
+        self.model = model.to(self.device)
         self.sequence_of_input = sequence_of_input
         self.output = [np.zeros(shape=(len(self.sequence_of_input), self.model.config.n_embd))
                        for i in range(self.model.config.n_head)]  # list of list of tensors (num_layers x num_text x size_hidden_layer)
+
 
     def extract_features(self):
         num_input = len(self.sequence_of_input)
         num_layers = self.model.config.n_layer
 
         for input_num, input in enumerate(self.sequence_of_input):
-            encoded_input = self.tokenizer(input, return_tensors="pt")
+            encoded_input = self.tokenizer(input, return_tensors="pt").to(self.device)
             output = self.model(**encoded_input, output_hidden_states=True)  #tuple of tensors, one for each layer https://huggingface.co/docs/transformers/main_classes/output
 
             for layer_num, layer_output in enumerate(output.hidden_states[1:]):  # first is input embedding, no attn
                 # get embedding of final token of sequence
                 final_token_embedding = layer_output[0][-1]  # grabbing final token of input_num, layer_num
-                self.output[layer_num][input_num] = final_token_embedding.detach().numpy()
-                # os.system("cls")
+                if self.device=="cpu":
+                    self.output[layer_num][input_num] = final_token_embedding.detach().numpy()
+                else:
+                    self.output[layer_num][input_num] = final_token_embedding.cpu().detach().numpy()
                 print("Extracting input {}/{} at layer {}/{}".format(input_num+1, num_input,
                                                                          layer_num+1, num_layers), end="\r")
 
