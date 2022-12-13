24f3b25af81b6988922c67b9996fe0f407a27b0f
chialinkhern <chialinkhern@gmail.com>
2022-12-13 10:47:55 -0600

    finished

--- create_control_tasks.py
+++ create_control_tasks.py
@@ -7,12 +7,14 @@ from ControlTaskGenerator import *
 from transformers import GPT2TokenizerFast
 
 
-l = ["text of oaskdo", "instructor's name", "scan below for classroom documentation.", "oh oaskdo", "my name"]
+data = load_dataset("sem_eval_2018_task_1", "subtask5.english")
+splits = ["train", "test", "validation"]
+
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-large")
-gen = ControlTaskGenerator(tokenizer=tokenizer)
-gen.fit(l)
-print(gen.transform(l[0:1]))
-print()
-print(gen.transform(l[0:2]))
-print()
-print(gen.transform(l[0:3]))
+control_task_generator = ControlTaskGenerator(tokenizer=tokenizer)
+
+for split in splits:
+    list_of_tweets = data[split]["Tweet"]
+    control_task_generator.fit(list_of_tweets)
+    control_task = control_task_generator.transform(list_of_tweets)
+    np.savetxt("tweet_labels/{}/control_labels.csv".format(split), control_task, delimiter=",")
